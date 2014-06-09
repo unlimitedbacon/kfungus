e0bf00dc7cb9eceb837f41c0064bddd5d555e38e
Tyler Anderson <unlimitedbacon@gmail.com>
2014-06-09 01:59:23 -0700

    Ghost has shape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import kivy
 kivy.require('1.8.0')
 
+from random import randint
+
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
@@ -16,6 +18,7 @@ from kivy.graphics import Color, Rectangle
 
 from tetrominoes import tetro
 
+# Set window to same resolution as Nexus 4
 Config.set('graphics','width','1280')
 Config.set('graphics','height','768')
 
@@ -24,20 +27,24 @@ grid_size_y = 20
 
 sprite_size = 32
 
+# Shows a single lonesome tetromino
 class TetroGrid(GridLayout):
-	def setup(self, tetromino):
+	def setup(self, tetromino, fungus, ftype='norm'):
 		self.col_default_width = sprite_size
 		self.row_default_height = sprite_size
 		self.rows = len(tetromino)
 		self.cols = len(tetromino[0])
 		self.size = ( sprite_size*self.cols, sprite_size*self.rows )
+		# Add each block to the GridLayout
 		for y in range(self.rows):
 			for x in range(self.cols):
 				b = GridBlock()
 				b.background = False
+				b.ftype = ftype
 				if tetromino[y][x]:
 					b.fungus = 'Green'
 
+					# Join neighboring blocks
 					neighbors = ''
 					# Up
 					if y > 0:
@@ -60,29 +67,44 @@ class TetroGrid(GridLayout):
 				self.add_widget(b)
 
 
+# A single grid block containing either a fungus or nothing
 class GridBlock(FloatLayout):
 	fungus = StringProperty('None')
+	ftype = StringProperty('norm')
 	neighbors = StringProperty('')
 	background = BooleanProperty(True)
 	sprite = ObjectProperty(None)
 	grid_background = ObjectProperty(None)
 
 	def on_fungus(self, instance, value):
-		if value != 'None':
-			self.sprite.source = 'atlas://'+value+'/home/x'
-		else:
-			self.sprite.source = 'blank.png'
+		self.update_sprite()
+		#if value != 'None':
+		#	self.sprite.source = 'atlas://'+value+'/home/x'
+		#else:
+		#	self.sprite.source = 'blank.png'
+	def on_ftype(self, instance, value):
+		self.update_sprite()
+		#self.sprite.source = 'atlas://'+self.fungus+'/'+self.ftype+'/'+self.neighbors
 	def on_neighbors(self, instance, value):
-		if self.fungus != 'None':
-			if len(value) > 0:
-				self.sprite.source = 'atlas://'+self.fungus+'/home/'+value
-			else:
-				self.sprite.source = 'atlas://'+self.fungus+'/home/x'
+		self.update_sprite()
+		#if self.fungus != 'None':
+		#	if len(value) > 0:
+		#		self.sprite.source = 'atlas://'+self.fungus+'/home/'+value
+		#	else:
+		#		self.sprite.source = 'atlas://'+self.fungus+'/home/x'
 	def on_background(self, instance, value):
 		if value:
 			self.grid_background.source = 'Grid/block.png'
 		else:
 			self.grid_background.source = 'blank.png'
+	def update_sprite(self):
+		if self.fungus == 'None':
+			self.sprite.source = 'blank.png'
+		else:
+			if self.neighbors == '':
+				self.sprite.source = 'atlas://'+self.fungus+'/'+self.ftype+'/x'
+			else:
+				self.sprite.source = 'atlas://'+self.fungus+'/'+self.ftype+'/'+self.neighbors
 
 class GameGridView(Scatter):
 	gglayout = GridLayout(cols = grid_size_x,
@@ -134,19 +156,25 @@ class HorizLine(Widget):
 	pass
 
 class Ghost(Scatter):
+	ghost_grid = ObjectProperty(None)
+
+	def setup(self, np, color):
+		self.ghost_grid.setup( np, color, 'ghost' )
+		self.ghost_grid.pos = (0,0)
+			
 	def on_touch_up(self, touch):
 		super(Ghost, self).on_touch_up(touch)
 		self.parent.place_block(*touch.pos)
 		self.parent.remove_widget(self)
+
 	def on_touch_move(self, touch):
 		# Snap when inside grid
 		g_x, g_y = self.parent.ggview.to_local(*touch.pos)
 		s_x, s_y = self.parent.ggview.gglayout.size
 		scale = self.parent.ggview.scale
 		if g_x > 0 and g_x < s_x and g_y > 0 and g_y < s_y:
-			# I have absolutely no idea what is up with the 34 pixel offset
-			self.x = int(g_x/sprite_size)*sprite_size*scale + self.parent.ggview.x - 34*scale
-			self.y = int(g_y/sprite_size)*sprite_size*scale + self.parent.ggview.y - 34*scale
+			self.x = int(g_x/sprite_size)*sprite_size*scale + self.parent.ggview.x
+			self.y = int(g_y/sprite_size)*sprite_size*scale + self.parent.ggview.y
 		else:
 			self.center = touch.pos 
 
@@ -157,10 +185,12 @@ class PlayerWidget(Widget):
 	new_piece_box = ObjectProperty(None)
 
 class FungusGame(FloatLayout):
-	grid = []
 	player1_panel = ObjectProperty(None)
 	ggview = ObjectProperty(None)
 	ghost = None
+	grid = []
+	current_player = 'Green'
+	new_piece = tetro[0]
 
 	def new_game(self):
 		# Initialize matrix of block objects.
@@ -173,9 +203,12 @@ class FungusGame(FloatLayout):
 				self.grid[y].append(GridBlock())
 
 		self.grid[5][5].fungus = 'Green'
+		self.grid[5][5].ftype = 'home'
 		self.ggview.setup(self.grid)
 
-		self.player1_panel.new_piece_grid.setup(tetro[3])
+		# Choose random starting piece
+		self.new_piece = tetro[ randint(0,9) ]
+		self.player1_panel.new_piece_grid.setup( self.new_piece, self.current_player, 'home' )
 	
 	def on_touch_down(self, touch):
 		super(FungusGame, self).on_touch_down(touch)
@@ -185,11 +218,13 @@ class FungusGame(FloatLayout):
 			self.ghost = Ghost()
 			self.ghost.center = new_piece_box.center
 			self.ghost.scale = self.ggview.scale
+			self.ghost.setup( self.new_piece, self.current_player )
 			self.add_widget(self.ghost)
 
 	def place_block(self, x, y):
 		x, y = self.ggview.global_coords_to_block( x, y )
 		self.grid[y][x].fungus = 'Green'
+		self.grid[y][x].ftype = 'home'
 		self.update_neighbors(self.grid)
 	
 	def update_neighbors(self, grid):
